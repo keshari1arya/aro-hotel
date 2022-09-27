8c3d5c3ab1874946358df48721b86e05dacfd50b
Arya <keshari1arya@gmail.com>
2022-09-27 14:21:46 +0530

    Basic API call established

--- aro-hotel.UI/ClientApp/src/app/app.module.ts
+++ aro-hotel.UI/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HotelListComponent } from './hotel/hotel-list/hotel-list.component';
+import { HotelDetailsComponent } from './hotel/hotel-details/hotel-details.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +18,16 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    HotelListComponent,
+    HotelDetailsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: HotelListComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
