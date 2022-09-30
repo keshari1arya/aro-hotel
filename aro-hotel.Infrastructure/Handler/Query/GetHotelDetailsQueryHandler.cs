2fb1214d556ce8584a3cfab1c06d95f2e52e082f
Arya <keshari1arya@gmail.com>
2022-09-30 20:14:29 +0530

    Query included

---
+++ aro-hotel.Infrastructure/Handler/Query/GetHotelDetailsQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using aro_hotel.Domain.Model;
+using aro_hotel.Infrastructure.DTO.Response;
+using aro_hotel.Infrastructure.Query;
+using aro_hotel.Infrastructure.Repository;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace aro_hotel.Infrastructure.Handler.Query
+{
+    public class GetHotelDetailsQueryHandler : IRequestHandler<GetHotelDetailsQuery, HotelResponse>
+    {
+        private readonly IRepository<Hotel> repository;
+        private readonly IMapper mapper;
+
+        public GetHotelDetailsQueryHandler(IRepository<Hotel> repository, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.repository = repository;
+        }
+
+        public async Task<HotelResponse> Handle(GetHotelDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var hotel = await this.repository.Entity()
+                .Include(x => x.Address)
+                .Include(x => x.Rooms)
+                    .ThenInclude(x => x.Multimedias)
+                .Include(x => x.Rooms)
+                    .ThenInclude(x => x.RoomType)
+                .Include(x => x.Rooms)
+                    .ThenInclude(x => x.RoomFacilityXREFs)
+                        .ThenInclude(x => x.Facility)
+                .Include(x => x.Multimedias)
+                .Include(x => x.HotelFacilityXREFs)
+                    .ThenInclude(x => x.Facility)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id);
+            return this.mapper.Map<HotelResponse>(hotel);
+        }
+    }
+}
+
